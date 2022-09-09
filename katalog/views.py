654da769620cc6552e4be5a992c9d2199b5eee66
jihansyafakamila <jihansyafakamila@gmail.com>
2022-09-10 01:33:07 +0700

    Memodifikasi beberapa file

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_katalog_item,
+        'nama': 'Jihan Syafa Kamila',
+        'NPM' : '2106751303'
+    }
+    return render(request, "katalog.html", context)
